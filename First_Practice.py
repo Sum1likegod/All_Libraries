3f60aa6811d04a08faf12ff2e210ae3f47a45832
Sum1likegod <it2037@global.org.in>
2026-06-30 14:14:03 +0530

    1. Continuing the learning.

--- First_Practice.py
+++ First_Practice.py
@@ -1,7 +1,11 @@
 import numpy as np
 
-arr_1 = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+# arr_1 = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
 
-print(arr_1[:,2:4])
+# print(arr_1[:,2:4])
+
+# print(*range(1, 10, 2),)
 
 
+##29-06 Arange function is used to create an array of evenly spaced values within a given interval. It takes three parameters: start, stop, and step. The start parameter specifies the starting value of the sequence, the stop parameter specifies the end value (exclusive), and the step parameter specifies the difference between each consecutive value in the sequence.
+print(np.arange(2, 10, 2))
